2ff61ef63f2369a31016d26e85a82a193e9bf717
Swati <swatimishra@dhcp-rhodes-2438.eduroam.cornell.edu>
2018-10-31 13:43:51 -0400

    again commit

---
+++ StimulusQues.js
@@ -0,0 +1,293 @@
+var StimulusImQues = [
+						//Question 1 -Text
+					   {fileID: 1,
+					   SelOptions:[],
+					   Answer:["ZKW4"],
+					   QuesText:"Type the characters you see above",
+					   Label:"Text"
+					  },
+					  
+					  //Question 2 -Text
+					  {fileID: 2,
+					   SelOptions:[],
+					   Answer:["BMVHKY"],
+					   QuesText:"Type the characters you see above",
+					   Label:"Text"
+					  },
+					  
+					  //Question 3 -Text
+					  {fileID:3,
+					   SelOptions:[],
+					   Answer:["7d6bf"],
+					   QuesText:"Type the characters you see above",
+					   Label:"Text"
+					  },
+					  
+					  //Question 4 -Text
+					  {fileID:4,
+					   SelOptions:[],
+					   Answer:["3-2 parks"],
+					   QuesText:"Type the characters you see above",
+					   Label:"Text"
+					  },
+					  
+					  //Question 5 -Text
+					  {fileID:5,
+					   SelOptions:[],
+					   Answer:["advses"],
+					   QuesText:"Type the characters you see above",
+					   Label:"Text"
+					  },
+					  
+					  //Question 6 -Text
+					  {fileID:6,
+					   SelOptions:[],
+					   Answer:["RAE3"],
+					   QuesText:"Type the characters you see above",
+					   Label:"Text"
+					  },
+					  
+					  //Question 7 -Text
+					  {fileID:7,
+					   SelOptions:[],
+					   Answer:["3nc9z"],
+					   QuesText:"Type the characters you see above",
+					   Label:"Text"
+					  },
+					  
+					  //Question 8 -Text
+					  {fileID:8,
+					   SelOptions:[],
+					   Answer:["2pvcb"],
+					   QuesText:"Type the characters you see above",
+					   Label:"Text"
+					  },
+					  
+					  //Question 9 -Text
+					  {fileID:9,
+					   SelOptions:[],
+					   Answer:["quxg4h"],
+					   QuesText:"Type the characters you see above",
+					   Label:"Text"
+					  }
+					  ,
+					  //Question 10 -Text
+					  {fileID:10,
+					   SelOptions:[],
+					   Answer:["trustother"],
+					   QuesText:"Type the characters you see above",
+					   Label:"Text"
+					  }
+					  ,
+					  //Question 11 -Text
+					  {fileID:11,
+					   SelOptions:[],
+					   Answer:["slythygomi"],
+					   QuesText:"Type the characters you see above",
+					   Label:"Text"
+					  }
+					  ,
+					  //Question 12 -Text
+					  {fileID:12,
+					   SelOptions:[],
+					   Answer:["pmymku"],
+					   QuesText:"Type the characters you see above",
+					   Label:"Text"
+					  }
+					  ,
+					  //Question 13 -Image
+					  {fileID:13,
+					   SelOptions:[],
+					   Answer:[1,2,5,8],
+					   QuesText:"Select all images with Waffles",
+					   Label:"Image"
+					  }
+					  ,
+					  //Question 14 -Image
+					  {fileID:14,
+					   SelOptions:[],
+					   Answer:[2,7],
+					   QuesText:"Select all the drinks",
+					   Label:"Image"
+					  }
+					  ,
+					  //Question 15 -Image
+					  {fileID:15,
+					   SelOptions:[],
+					   Answer:[1,2,5,6,8],
+					   QuesText:"Select all images below that match the above image",
+					   Label:"Image"
+					  }
+					  ,
+					  //Question 16 -Image
+					  {fileID:16,
+					   SelOptions:[],
+					   Answer:[],
+					   QuesText:"Select the image that matches the label N",
+					   Label:"Image"
+					  }
+					  ,
+					  //Question 17 -Image
+					  {fileID:17,
+					   SelOptions:[],
+					   Answer:[5,6,8],
+					   QuesText:"Select images that have cat",
+					   Label:"Image"
+					  }
+					  ,
+					  //Question 18 -Image
+					  {fileID:18,
+					   SelOptions:[],
+					   Answer:[2,9],
+					   QuesText:"Select images with waterfalls",
+					   Label:"Image"
+					  }
+					  ,
+					  //Question 19 -Image
+					  {fileID:19,
+					   SelOptions:[],
+					   Answer:[3,4,9],
+					   QuesText:"Select picture with label of transportation",
+					   Label:"Image"
+					  }
+					  ,
+					  //Question 20 -Image
+					  {fileID:20,
+					   SelOptions:[],
+					   Answer:[5,7,8],
+					   QuesText:"Select images with trees",
+					   Label:"Image"
+					  }
+					  ,
+					  //Question 21 -Image
+					  {fileID:21,
+					   SelOptions:[],
+					   Answer:[1,2,5,9],
+					   QuesText:"Select all images with mountains",
+					   Label:"Image"
+					  }
+					  ,
+					  //Question 22 -Image
+					  {fileID:22,
+					   SelOptions:[],
+					   Answer:8,
+					   QuesText:"Select the image with raw apples",
+					   Label:"Image"
+					  }
+					  ,
+					  //Question 23 -Image
+					  {fileID:23,
+					   SelOptions:[],
+					   Answer:[4,6],
+					   QuesText:"Select all images with pasta or noodles",
+					   Label:"Image"
+					  }
+					  ,
+					  //Question 24 -Image
+					  {fileID:24,
+					   SelOptions:[],
+					   Answer:[3,5],
+					   QuesText:"Select all images with peanuts",
+					   Label:"Image"
+					  }
+					  ,
+					  //Question 25 -Illusion
+					  {fileID:25,
+					   SelOptions:[],
+					   Answer:[3,4,6],
+					   QuesText:"Select all the different people you see in the image",
+					   Label:"Illusion"
+					  }
+					  ,
+					  //Question 26 -Illusion
+					  {fileID:26,
+					   SelOptions:[],
+					   Answer:[1,3,4,7],
+					   QuesText:"Select all the different animals you see in the image",
+					   Label:"Illusion"
+					  }
+					  ,
+					  //Question 27 -Illusion
+					  {fileID:27,
+					   SelOptions:[],
+					   Answer:[1,2,5,8,9],
+					   QuesText:"Select all living things you see in the image",
+					   Label:"Illusion"
+					  }
+					  ,
+					  //Question 28 -Illusion
+					  {fileID:28,
+					   SelOptions:[],
+					   Answer:[4,5,6],
+					   QuesText:"Select all living things you see in the image",
+					   Label:"Illusion"
+					  }
+					  ,
+					  //Question 29 -Illusion
+					  {fileID:29,
+					   SelOptions:[],
+					   Answer:[2,5],
+					   QuesText:"Select all things that you see in the image",
+					   Label:"Illusion"
+					  }
+					  ,
+					  //Question 30 -Illusion
+					  {fileID:30,
+					   SelOptions:[],
+					   Answer:[3,6,8],
+					   QuesText:"Select all things you see in the image",
+					   Label:"Illusion"
+					  }
+					  ,
+					  //Question 31 -Illusion
+					  {fileID:31,
+					   SelOptions:[],
+					   Answer:[1,5,7],
+					   QuesText:"Select all animals you in the image",
+					   Label:"Illusion"
+					  }
+					  ,
+					  //Question 32 -Illusion
+					  {fileID:32,
+					   SelOptions:[1,4,6,8],
+					   Answer:[],
+					   QuesText:"Select all that you see in the image",
+					   Label:"Illusion"
+					  }
+					  ,
+					  //Question 33 -Illusion
+					  {fileID:33,
+					   SelOptions:[],
+					   Answer:[2,4,7],
+					   QuesText:"Select all things you see in the image",
+					   Label:"Illusion"
+					  }
+					  ,
+					  //Question 34 -Illusion
+					  {fileID:34,
+					   SelOptions:[],
+					   Answer:[2,3,7],
+					   QuesText:"Select all that you see in the image",
+					   Label:"Illusion"
+					  }
+					  ,
+					  //Question 35 -Illusion
+					  {fileID:35,
+					   SelOptions:[],
+					   Answer:[1,2,7],
+					   QuesText:"Select all that you see in the image",
+					   Label:"Illusion"
+					  },
+					  
+					  //Question 36 -Illusion
+					  {fileID:36,
+					   SelOptions:[],
+					   Answer:[1,2,5,6],
+					   QuesText:"Select all that you see in the image",
+					   Label:"Illusion"
+					  }
+					];
+
+
+
+
